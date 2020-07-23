9ba9f1c4926fad6e042e78a5e0ecafb24bc174b1
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 18:01:20 -0500

    sanitizer params changed

--- app/controllers/candidates/paramater_sanitizer_controller.rb
+++ app/controllers/candidates/paramater_sanitizer_controller.rb
@@ -1,22 +1,26 @@
-class Candidates::ParamaterSanitizerController < Devise::ParameterSanitizer
-  def initialize(*)
-    super
-    permit(:account_update, keys: [
-                                   :organization,
-                                   :sector,
-                                   :name,
-                                   :last_name,
-                                   :id,
-                                   :place_of_birth,
-                                   :place_of_residence,
-                                   :address,
-                                   :residence_area_type,
-                                   :phone,
-                                   :secondary_phone,
-                                   :email,
-                                   :education_level,
-                                   :expertise,
-                                   :value_proposition,
-                                   :disclaimer ])
-  end
-end
+# class Candidates::ParamaterSanitizerController < Devise::ParameterSanitizer
+#   # def initialize(*)
+#   #   super
+#   #   permit(:account_update, keys: [
+#   #                                  :organization,
+#   #                                  :sector,
+#   #                                  :name,
+#   #                                  :last_name,
+#   #                                  :tax_id_number,
+#   #                                  :place_of_birth,
+#   #                                  :place_of_residence,
+#   #                                  :address,
+#   #                                  :residence_area_type,
+#   #                                  :phone,
+#   #                                  :secondary_phone,
+#   #                                  :email,
+#   #                                  :education_level,
+#   #                                  :expertise,
+#   #                                  :value_proposition,
+#   #                                  :disclaimer,
+#   #                                  :document,
+#   #                                  :confirmation_letter,
+#   #                                  :organization_confirmation_letter,
+#   #                                  :photo ])
+#   # end
+# end
