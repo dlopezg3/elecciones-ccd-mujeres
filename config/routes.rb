ef76e3e87ef39a5faf805edd0e35e0e891ea60d9
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 08:29:33 -0500

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :voters
+  devise_for :candidates, controllers: { registrations: "candidates/registrations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
