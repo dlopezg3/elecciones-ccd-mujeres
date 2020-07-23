2f633e1e5c99b8c78807a9239272f230577b60ce
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 18:02:01 -0500

    active storage para candidatos

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -5,4 +5,9 @@ class Candidate < ApplicationRecord
          :recoverable, :rememberable, :validatable
   belongs_to :organization, optional: true
   belongs_to :sector, optional: true
+
+  has_one_attached :photo
+  has_one_attached :document
+  has_one_attached :confirmation_letter
+  has_one_attached :organization_confirmation_letter
 end
