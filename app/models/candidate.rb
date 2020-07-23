2625539da1802ccb20ed96b627c228cb2217ccb8
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 08:30:28 -0500

    models creation

---
+++ app/models/candidate.rb
@@ -0,0 +1,8 @@
+class Candidate < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  belongs_to :organization, optional: true
+  belongs_to :sector, optional: true
+end
