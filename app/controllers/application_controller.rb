a8bdfe70d48b377eb0a85bd9c99ba6de0589f2e7
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 08:25:53 -0500

    logic for candidates registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  devise_group :entity, contains: [:candidate, :voter]
+
+
+  protected
+
+  def after_sign_up_path_for(resource_or_scope)
+    welcome_index_path
+  end
+
+  def devise_parameter_sanitizer
+    if resource_class == Candidate
+      Candidates::ParamaterSanitizerController.new(Candidate, :candidate, params)
+    else
+      super # Use the default one
+    end
+  end
+
 end
