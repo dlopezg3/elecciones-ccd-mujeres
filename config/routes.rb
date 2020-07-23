a94a4a1765dd549518f1e22f7adf24e92a4184a1
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-23 18:05:00 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'candidates/show'
   devise_for :voters
   devise_for :candidates, controllers: { registrations: "candidates/registrations" }
+
+  resources :candidates, only: [:show]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
