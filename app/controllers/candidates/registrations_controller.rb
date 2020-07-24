f55becea8dd3f635495e53cf5d0e1ba1a081ec24
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-24 15:42:33 -0500

    attributes of candidacy moved to candidacies self controller

--- app/controllers/candidates/registrations_controller.rb
+++ app/controllers/candidates/registrations_controller.rb
@@ -1,45 +1,5 @@
 class Candidates::RegistrationsController < Devise::RegistrationsController
-  def edit
-    @candidate = current_candidate
-  end
-
-  def update
-    @candidate = current_candidate
-    if @candidate.update(candidate_params)
-      flash[:notice] = "Tu candidatura ha sido actualizado con éxito"
-      redirect_to candidate_path(@candidate)
-    else
-      flash.now[:alert] = "Por favor corrige los campos con error e intenta nuevamente"
-      render 'edit'
-    end
-  end
-
-  protected
-
   def after_sign_up_path_for(resource)
-     edit_candidate_registration_path
-  end
-
-  def candidate_params
-    params.require(:candidate).permit(:organization,
-                                   :sector,
-                                   :name,
-                                   :last_name,
-                                   :tax_id_number,
-                                   :place_of_birth,
-                                   :place_of_residence,
-                                   :address,
-                                   :residence_area_type,
-                                   :phone,
-                                   :secondary_phone,
-                                   :email,
-                                   :education_level,
-                                   :expertise,
-                                   :value_proposition,
-                                   :disclaimer,
-                                   :document,
-                                   :confirmation_letter,
-                                   :organization_confirmation_letter,
-                                   :photo)
+    new_candidacy_path
   end
 end
