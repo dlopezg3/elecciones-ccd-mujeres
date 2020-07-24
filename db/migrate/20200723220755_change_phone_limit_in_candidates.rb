0b77b6217cc6e1afab87d0ffc1dc32ea59d6f601
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-24 07:35:02 -0500

    change onn migration secondary phone integer type

--- db/migrate/20200723220755_change_phone_limit_in_candidates.rb
+++ db/migrate/20200723220755_change_phone_limit_in_candidates.rb
@@ -1,5 +1,6 @@
 class ChangePhoneLimitInCandidates < ActiveRecord::Migration[6.0]
   def change
     change_column :candidates, :phone, :integer, limit: 8
+    change_column :candidates, :secondary_phone, :integer, limit: 8
   end
 end
