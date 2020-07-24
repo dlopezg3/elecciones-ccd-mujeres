4bf0db454dbcb75b6834cf32606c8aac97ac43df
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-24 15:44:20 -0500

    attributes from candidates moved to candidacies

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,11 +3,6 @@ class Candidate < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :organization, optional: true
-  belongs_to :sector, optional: true
 
-  has_one_attached :photo
-  has_one_attached :document
-  has_one_attached :confirmation_letter
-  has_one_attached :organization_confirmation_letter
+  has_one :candidacies, dependent: :destroy
 end
