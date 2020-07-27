dfe1896be993930d0702efe2162789ef56592bac
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-27 16:06:00 -0500

    send email after candidacy creation

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -6,4 +6,12 @@ class Candidacy < ApplicationRecord
   has_one_attached :document
   has_one_attached :confirmation_letter
   has_one_attached :organization_confirmation_letter
+
+  after_create :send_confirmation_email
+
+  private
+
+  def send_confirmation_email
+    CandidacyMailer.with(candidacy: self, candidate: self.candidate).confirmation.deliver_now
+  end
 end
