f4c86cd267d45dede09f362063405aa1b309001e
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-28 19:00:53 -0500

    campos adicionanles en el new y edit del form

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -6,8 +6,29 @@ class Candidacy < ApplicationRecord
   has_one_attached :document
   has_one_attached :confirmation_letter
   has_one_attached :organization_confirmation_letter
+  has_one_attached :autonomus_confirmation_letter
 
   after_create :send_confirmation_email
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :tax_id_number, presence: true
+  validates :tax_id_number, presence: true
+  validates :phone, presence: true
+  validates :expertise, presence: true
+  validates :organization, presence: true
+  validates :value_proposition, presence: true
+  validates :photo, presence: true
+  validates :document, presence: true
+  validates :confirmation_letter, presence: true
+  validates :organization_confirmation_letter, presence: true
+  validates :disclaimer, acceptance: { message: 'Debes aceptar los términos y condiciones' }
+
+  def initials
+    return if name.blank?
+
+    "#{name} #{last_name}".split(" ").map { |word| word[0] }.reduce(:+)[0..1]
+  end
 
   private
 
