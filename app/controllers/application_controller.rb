b49b49277b1688b36d6616fca13414e28d4bd898
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-02 18:32:15 -0500

    dinamic autentication based on user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   devise_group :entity, contains: [:candidate, :voter]
-  before_action :authenticate_candidate!
+  before_action :authenticate!
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def user_not_authorized
     flash[:alert] = "No estás authorizado para realizar esta acción"
     if current_candidate
@@ -18,11 +19,23 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def authenticate!
+    if current_entity == current_admin
+      :authenticate_admin!
+    elsif current_entity == current_candidate
+      :authenticate_candidate!
+    end
+  end
+
   protected
 
   def after_sign_in_path_for(resource_or_scope)
-    candidacy_path(current_candidate.candidacy) unless current_candidate.candidacy.nil?
-    new_candidacy_path
+    if resource_or_scope.instance_of? Candidate
+      candidacy_path(current_candidate.candidacy) unless current_candidate.candidacy.nil?
+      new_candidacy_path
+    else
+      super
+    end
   end
 
   def skip_pundit?
