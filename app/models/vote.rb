f6c20a30248243d0351445aa22ad8cbeebee9e20
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-20 16:06:00 -0500

    validations for email and phone for email on vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,8 @@ class Vote < ApplicationRecord
   validates :voter_tid, presence: true, uniqueness: true
   validates :voter_email, presence: true, uniqueness: true
   validates :voter_phone, presence: true, uniqueness: true
+
+  validates_format_of :voter_tid, with: /\A((\d{8})|(\d{10})|(\d{11})|(\d{6}-\d{5}))?\z/
+  validates_format_of :voter_email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
