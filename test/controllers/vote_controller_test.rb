96141fc0b7ae0c0bc2008e7609272d5b8e070a97
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-20 09:40:41 -0500

    test files

---
+++ test/controllers/vote_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class VoteControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get vote_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get vote_create_url
+    assert_response :success
+  end
+
+end
