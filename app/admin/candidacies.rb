1c002257c36f24d924f1b64d06580ae0cdb125c5
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-31 09:08:42 -0500

    add on admin candidacies index: filters, actions at the end, email on the xls and authorization to edit and delete

--- app/admin/candidacies.rb
+++ app/admin/candidacies.rb
@@ -1,16 +1,15 @@
 ActiveAdmin.register Candidacy do
 
-  actions :index, :show, :update, :edit
+  actions :index, :show, :update, :edit, :destroy
   config.sort_order = 'created_at_desc'
 
+  filter :name
+  filter :last_name
+  filter :validated
   filter :sector_name, as: :select, collection: -> {Sector.pluck(:name)}, label: "Sector"
 
   index do
     index_column
-    actions dropdown: true do |candidacy|
-        admin_candidacy_path(candidacy)
-    end
-
     column :created_at
     column :sector
     column :organization
@@ -27,16 +26,22 @@ ActiveAdmin.register Candidacy do
     column("Aval de la organización") { |candidacy| candidacy.cloudinary_url(candidacy.confirmation_letter) }
     column("Certificado de existencia de la organización") { |candidacy| candidacy.cloudinary_url(candidacy.organization_confirmation_letter) }
     column("Carta aceptación sectores autónomos") { |candidacy| candidacy.cloudinary_url(candidacy.autonomus_confirmation_letter) }
+    column :validated
+    column :comments
+    actions
   end
 
   xls(i18n_scope: [:activerecord, :attributes, :candidacy],
       header_format: { weight: :bold }) do
 
     only_columns :name, :last_name, :organization, :tax_id_number, :place_of_birth, :place_of_residence, :residence_area_type, :phone, :secondary_phone, :expertise, :value_proposition
+    column("email") { |candidacy| candidacy.candidate.email }
     column("photo") { |candidacy| candidacy.photo.service_url if candidacy.photo.attached? }
     column("document") { |candidacy| candidacy.document.service_url if candidacy.document.attached? }
     column("confirmation_letter") { |candidacy| candidacy.confirmation_letter.service_url if candidacy.confirmation_letter.attached? }
     column("organization_confirmation_letter") { |candidacy| candidacy.organization_confirmation_letter.service_url if candidacy.organization_confirmation_letter.attached? }
     column("autonomus_confirmation_letter") { |candidacy| candidacy.autonomus_confirmation_letter.service_url if candidacy.autonomus_confirmation_letter.attached? }
   end
+
+  permit_params :name, :last_name, :tax_id_number, :place_of_birth, :place_of_residence, :address, :residence_area_type, :phone, :secondary_phone, :education_level, :expertise, :value_proposition, :organization, :validated, :comments
 end
