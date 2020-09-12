f8867b6c90afab11cced62ca5b7f12ceb80952ed
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-11 19:29:10 -0500

    method controller for confirmation page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate!, only: [:home]
+  skip_before_action :authenticate!, only: [:home, :confirmation]
 
   def home
+  end
+
+  def vote_confirmation
   end
 end
