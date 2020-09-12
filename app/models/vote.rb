32769cbf2ec035960218bb32d1544d0a9ad6a62c
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-11 19:32:07 -0500

    vote belongs to municipio and sector

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :candidacy
+  belongs_to :municipality
+  belongs_to :sector
 
   validates :voter_full_name, presence: true
   validates :voter_tid, presence: true, uniqueness: true
@@ -8,5 +10,4 @@ class Vote < ApplicationRecord
 
   validates_format_of :voter_tid, with: /\A((\d{8})|(\d{10})|(\d{11})|(\d{6}-\d{5}))?\z/
   validates_format_of :voter_email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
 end
