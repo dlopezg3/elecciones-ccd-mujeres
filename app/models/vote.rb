cc777901f92663d8410237a8b63d6c7b1b9bcfd8
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-14 10:06:58 -0500

    No validation for email

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,7 @@ class Vote < ApplicationRecord
 
   validates :voter_full_name, presence: true
   validates :voter_tid, presence: true, uniqueness: true
-  validates :voter_email, presence: true, uniqueness: true
+  # validates :voter_email, presence: true, uniqueness: true
   validates :voter_phone, presence: true, uniqueness: true
 
   validates_format_of :voter_tid, with: /\A((\d{8})|(\d{10})|(\d{11})|(\d{6}-\d{5}))?\z/
