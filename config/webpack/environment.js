747058cab3f9555502a4fe0cae408d289552b06a
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-14 11:10:59 -0500

    jquery bug fixed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const custom = {
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
+    }
+  }
+}
+
+environment.config.merge(custom)
