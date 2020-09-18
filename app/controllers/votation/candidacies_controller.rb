de98ebd6b83cf5cbc74f410cf49d4edd80a9d767
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-18 13:44:30 -0500

    scope on elected only

--- app/controllers/votation/candidacies_controller.rb
+++ app/controllers/votation/candidacies_controller.rb
@@ -4,7 +4,7 @@ class Votation::CandidaciesController < ApplicationController
   before_action :set_sector, only: [:index]
 
   def index
-    @candidacies = Candidacy.where(sector: @sector).validated.order(:name)
+    @candidacies = Candidacy.where(sector: @sector).elected.order(:name)
   end
 
   def show
